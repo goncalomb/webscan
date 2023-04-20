3f1973d9c87bf1d4eea38396ba58d35fb2610883
Gonçalo MB <me@goncalomb.com>
2023-04-20 15:02:51 +0100

    Add export options
    Improves preview canvas adding options to export to photopea and to
    download the image.
    
    Also adds a standalone server configuration for production.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { ChangeEvent, MouseEvent, useCallback, useRef, useState } from 'react';
 import './Canvas.css';
 import { useCanvasContext } from './CanvasContext';
 
 export default function Canvas() {
-  const { canvasRef } = useCanvasContext();
+  const { canvasRef, notInitialized, exportDownload, exportPhotopea } = useCanvasContext();
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const [scaled, setScaled] = useState(true);
+  const [quality, setQuality] = useState(95);
+
+  const onChangeQuality = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setQuality(Math.min(100, Math.max(0, Number(e.target.value))));
+  }, []);
+
+  const onClickCanvas = useCallback((e: MouseEvent<HTMLCanvasElement>) => {
+    if (scaled) {
+      // zooming-in, adjust scroll position to match clicked point
+      const c = e.target as HTMLCanvasElement;
+      const dx = c.offsetWidth;
+      const dy = c.offsetHeight;
+      const px = e.nativeEvent.offsetX / dx;
+      const py = e.nativeEvent.offsetY / dy;
+      setTimeout(() => {
+        c.parentElement?.scrollTo((px * (c.offsetWidth - dx)), (py * (c.offsetHeight - dy)));
+      }, 0);
+    }
+    setScaled(s => !s);
+  }, [scaled]);
+
   return (
     <div className="Canvas">
-      <canvas ref={canvasRef}></canvas>
+      <div>
+        <canvas ref={canvasRef} className={scaled ? 'scaled' : ''} title="Click to Zoom." onClick={onClickCanvas}></canvas>
+      </div>
+      <fieldset disabled={notInitialized}>
+        Export:
+        {' '}
+        <select ref={selectRef} >
+          <option value="image/jpeg">JPEG</option>
+          <option value="image/png">PNG</option>
+        </select>
+        {' '}
+        <input
+          type="number"
+          title="Image Quality (%). JPEG only."
+          min={0}
+          max={100}
+          maxLength={5}
+          value={quality}
+          onChange={onChangeQuality}
+        />
+        {' '}
+        <button onClick={e => exportDownload(selectRef.current?.value || '', quality / 100)}>Download</button>
+        {' '}
+        <button onClick={e => exportPhotopea(selectRef.current?.value || '', quality / 100)}>Send to Photopea</button>
+      </fieldset>
     </div>
   );
 }
--- src/components/CanvasContext.tsx
+++ src/components/CanvasContext.tsx
@@ -1,9 +1,65 @@
-import React, { ReactNode, useCallback, useContext, useRef } from 'react';
+import React, { ReactNode, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { isNavigatorSupported } from '../utils';
+
+class PhotopeaWindowManager {
+
+  _w: Window | null = null;
+  _ready = false;
+  _queue: { type: string, data?: any }[] = [];
+
+  _handleMessage = ({ origin, source, data: { type } }: MessageEvent) => {
+    if (origin === window.origin && source === this._w) {
+      if (type === 'ready') {
+        this._ready = true;
+        this._process();
+      } else if (type === 'photopea') {
+        // ...
+      }
+    }
+  };
+
+  _process() {
+    if (this._w && !this._w.closed && this._ready) {
+      const w = this._w;
+      this._queue.forEach(m => w.postMessage(m), window.origin);
+      this._queue = [];
+    }
+  }
+
+  open() {
+    if (!this._w || this._w.closed) {
+      this._ready = false;
+      this._w = window.open('/photopea.html');
+      if (!this._w) {
+        alert('Failed to open window.');
+        return;
+      }
+    } else {
+      this._w.focus();
+    }
+  }
+
+  post(type: string, data?: any) {
+    this._queue.push({ type, data });
+    this._process();
+  }
+
+  connect() {
+    window.addEventListener('message', this._handleMessage);
+  }
+
+  disconnect() {
+    window.removeEventListener('message', this._handleMessage);
+  }
+}
 
 interface ICanvasContext {
   canvasRef: React.RefObject<HTMLCanvasElement>;
+  notInitialized: boolean;
   resetCanvas: (width: number, height: number) => void;
   putImageData: (data: ImageData, dy: number) => void;
+  exportDownload: (type: string, quality: number) => void;
+  exportPhotopea: (type: string, quality: number) => void;
 }
 
 const CanvasContext = React.createContext<ICanvasContext | null>(null);
@@ -18,10 +74,13 @@ export const useCanvasContext = () => {
 
 export default function CanvasContextProvider({ children }: { children: ReactNode }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [notInitialized, setNotInitialized] = useState(true);
   const ctxRef = useRef<CanvasRenderingContext2D>();
+  const photopeaManagerRef = useRef(new PhotopeaWindowManager());
 
   const resetCanvas = useCallback((width: number, height: number) => {
     if (canvasRef.current) {
+      setNotInitialized(false);
       canvasRef.current.width = width;
       canvasRef.current.height = height;
       ctxRef.current = canvasRef.current.getContext('2d') || undefined;
@@ -34,9 +93,65 @@ export default function CanvasContextProvider({ children }: { children: ReactNod
     }
   }, []);
 
+  const exportDownload = useCallback((type: string, quality: number) => {
+    if (canvasRef.current) {
+      const exts: { [k: string]: string } = {
+        'image/jpeg': 'jpg',
+        'image/png': 'png',
+      };
+      canvasRef.current.toBlob(blob => {
+        if (blob) {
+          let a = document.createElement('a');
+          a.href = URL.createObjectURL(blob);
+          a.download = `scan.${exts[type] || 'bin'}`;
+          a.click();
+          setTimeout(() => URL.revokeObjectURL(a.href), 0);
+        }
+      }, type, quality);
+    }
+  }, []);
+
+  const exportPhotopea = useCallback((type: string, quality: number) => {
+    if (canvasRef.current) {
+      photopeaManagerRef.current.open();
+      canvasRef.current.toBlob(blob => {
+        blob?.arrayBuffer().then(data => {
+          photopeaManagerRef.current.post('photopea', data);
+        });
+      }, type, quality);
+    }
+  }, []);
+
+  // draw canvas placeholder
+  useEffect(() => {
+    if (canvasRef.current) {
+      const w = 500, h = 150;
+      const text = isNavigatorSupported() ? 'Image Preview' : '¯\\_(ツ)_/¯';
+      canvasRef.current.width = w;
+      canvasRef.current.height = h;
+      const ctx = canvasRef.current.getContext('2d');
+      if (ctx) {
+        ctx.font = '40px sans-serif';
+        ctx.textBaseline = 'middle';
+        const m = ctx.measureText(text);
+        ctx.strokeText(text, (w / 2) - (m.actualBoundingBoxRight - m.actualBoundingBoxLeft) / 2, h / 2);
+      }
+    }
+  }, []);
+
+  // handle photopea window manager
+  useEffect(() => {
+    const manager = photopeaManagerRef.current;
+    manager.connect();
+    return () => {
+      manager.disconnect();
+    };
+  }, []);
+
   return (
     <CanvasContext.Provider value={{
-      canvasRef, resetCanvas, putImageData,
+      notInitialized, canvasRef,
+      resetCanvas, putImageData, exportDownload, exportPhotopea,
     }}>
       {children}
     </CanvasContext.Provider>
